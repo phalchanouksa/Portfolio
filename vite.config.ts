b656f7de3d23071bd3c9b2ff59ff0b5c1bf2e1cf
Ouksa <phalchanouksa@gmail.com>
2025-02-12 15:39:57 +0700

    change some config to work with github

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
